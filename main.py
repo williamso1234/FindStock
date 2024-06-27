4d9c8c80d38b81725cdebfab2a74637920c7fe82
williamso1234 <williamso1888@gmail.com>
2024-06-26 22:05:17 -0700

    changes made

--- main.py
+++ main.py
@@ -2,12 +2,29 @@ import yfinance as YahooFinance
 
 GetApplovinInformation = YahooFinance.Ticker("APP")
 
-history = GetApplovinInformation.history(period="1y")
+history = GetApplovinInformation.history(period="max")
 
-LOTY = history["Low"].min()
-DLOTY = history["Low"].idxmin()
-HOTY = history["High"].max()
-DHOTY = history["High"].idxmax()
+PSU = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120]
 
-print("The lowest price of the year was,", LOTY, "at the date of,", DLOTY)
-print("The highest price of the year was,", HOTY, "at the date of,", DHOTY)
+
+def latest_unlocked_psu(history, PSU):
+    unlocked_price = None
+    unlocked_date = None
+    for Price_point in PSU:
+        DaysInRow = 0
+        for date, price in history['Close'].items():
+            if price >= Price_point:
+                DaysInRow += 1
+                if DaysInRow >= 30:
+                    unlocked_price = Price_point
+                    unlocked_date = date
+                    break
+            else:
+                DaysInRow = 0
+        if DaysInRow < 30:
+            break
+    return unlocked_price, unlocked_date
+
+
+unlocked_price, unlocked_date = latest_unlocked_psu(history, PSU)
+print("The latest unlocked price price is:", unlocked_price, " at", unlocked_date )
